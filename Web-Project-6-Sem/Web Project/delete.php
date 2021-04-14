831b7223a16d6bdb4567cd287cf23d88737dc994
Sunmeet <snmt5620@gmail.com>
2021-04-14 09:41:58 +0530

    Create delete.php

---
+++ Web-Project-6-Sem/Web Project/delete.php
@@ -0,0 +1,14 @@
+<?php
+//including the database connection file
+require_once("config.php");
+ 
+//get id of the record from url using supergloabl variable $_GET['id']
+$id = $_GET['id'];
+ 
+//deleting the row from table
+$sql = "DELETE FROM `users` WHERE `users`.`id` = $id";
+$result = $mysqli->query($sql);
+
+//redirecting to the home page (index.php in our case)
+header("Location: index.php");
+?>
